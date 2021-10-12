31bf0faf2013601d2c63e822bec3a19e1dc823d3
Ксения Тапок <soulssfg@gmail.com>
2021-10-12 16:26:59 +0300

    first

---
+++ myHomework/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace myHomework
+{
+    partial class Homework
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Button enter;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Homework));
+            this.enter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // enter
+            // 
+            this.enter.AccessibleDescription = "";
+            this.enter.AccessibleName = "";
+            this.enter.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.enter.BackColor = System.Drawing.SystemColors.GrayText;
+            this.enter.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.enter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.enter.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.enter.Location = new System.Drawing.Point(0, 415);
+            this.enter.Name = "enter";
+            this.enter.Size = new System.Drawing.Size(752, 35);
+            this.enter.TabIndex = 0;
+            this.enter.Text = "Защищаться";
+            this.enter.UseVisualStyleBackColor = false;
+            this.enter.Click += new System.EventHandler(this.enter_Click);
+            // 
+            // Homework
+            // 
+            this.AccessibleName = "myHomework";
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(752, 450);
+            this.Controls.Add(this.enter);
+            this.Cursor = System.Windows.Forms.Cursors.PanNW;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Homework";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Witcher\'s job";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+        private void InitializeButton()
+        {
+                        
+        }
+        #endregion
+
+    }
+}
+
---
+++ myHomework/Task522.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myHomework
+{
+    class Task522
+    {
+
+		class him
+		{
+			public char nazv[50];
+			public int ves;
+			public char prov[30];
+			public float k;
+
+		bool polu()
+		{
+			if (strcmp(prov, "полупроводник") == 0)
+				if (k > 5)
+					return true;
+			return false;
+		}
+	};
+
+	class table
+	{
+		public:
+	him* t;
+		int n;
+
+		table(char name[])
+		{
+			n = 0;
+			char temp[255];
+			fstream f, f2;
+			f.open(name);
+
+			if (f)
+			{
+				while (!f.eof())
+				{
+					f.getline(temp, 255);
+					if (strcmp(temp, "") != 0)
+						n++;
+				}
+				f.close();
+			}
+
+			t = new him[n];
+			n = 0;
+
+			f2.open(name);
+			if (f2)
+			{
+				while (!f2.eof())
+				{
+					f2 >> t[n].nazv;
+					f2 >> t[n].ves;
+					f2 >> t[n].prov;
+					f2 >> t[n].k;
+					n++;
+				}
+				f2.close();
+			}
+		}
+
+		void print()
+		{
+			cout << "Все данные из файла " << endl;
+			for (int i = 0; i < n; i++)
+				cout << t[i].nazv << "\t  Уд. вес: " << t[i].ves << "\t" << t[i].prov << "\t Кол-во: " << t[i].k << "\n";
+		};
+
+		void find()
+		{
+			cout << "Полупроводники, которых более 5:" << endl;
+			for (int i = 0; i < n; i++)
+				if (t[i].polu())
+					cout << t[i].nazv << "\t Уд. вес: " << t[i].ves << "\n";
+		};
+
+	};
+
+	int main()
+	{
+		setlocale(LC_ALL, "RUSSIAN");
+		table tab("input.txt");
+		tab.print();
+		tab.find();
+		system("pause");
+		return 0;
+	}
+
+	}
+}
